26a619b31bc1fb7776b0f1cad2cafffa1a32bb00
gomljo <gomljo0307@naver.com>
2023-06-26 19:38:02 +0900

    refactor: 하드 코딩된 값 상수화

--- src/main/java/com/simpleaccountservice/controller/TransactionController.java
+++ src/main/java/com/simpleaccountservice/controller/TransactionController.java
@@ -30,7 +30,6 @@ public class TransactionController {
             @Valid @RequestBody UseBalance.Request request) throws InterruptedException {
 
         try {
-//            Thread.sleep(3000L);
             return UseBalance.Response.from(transactionService.useBalance(
                     request.getUserId(),
                     request.getAccountNumber(),
